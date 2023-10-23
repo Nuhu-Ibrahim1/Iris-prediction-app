6c6b039ce8302e7ea98b76443cea8ef550c3a879
Nuhu Ibrahim <140070173+Nuhu-Ibrahim1@users.noreply.github.com>
2023-10-23 19:26:19 +0100

    Add files via upload

---
+++ iris.py
@@ -0,0 +1,28 @@
+import streamlit as st
+import pickle
+import pandas as pd
+from PIL import Image
+import sklearn
+st.title('Iris Prediction App')
+image = Image.open('C:\\Users\\Dr. Ibrahim Nuhu\\Downloads\\pexels-saliha-sevim-7651066.jpg')
+st.image(image)
+iris_model = pickle.load( open('C:\\Users\\Dr. Ibrahim Nuhu\\OneDrive\\Desktop\\segment_knn.sav','rb'))
+def user_report():
+    sepal_length = st.sidebar.slider('sepal length',1.0,10.0,0.1)
+    sepal_width = st.sidebar.slider('sepal width',1.0,10.0,0.1)
+    petal_length = st.sidebar.slider('petal length',1.0,10.0,0.1)
+    petal_width = st.sidebar.slider('petal width',1.0,10.0,0.1)
+    data_report = {
+        'sepal_length': sepal_length,
+        'sepal_width': sepal_width,
+        'petal_length': petal_length,
+        'petal_width': petal_width
+    }
+    data = pd.DataFrame(data_report, index=[0])
+    return data
+user_data = user_report()
+st.write(user_data)
+prediction = iris_model.predict(user_data)
+st.success(prediction)
+if (prediction ==0):
+    st.success
